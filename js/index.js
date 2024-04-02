fec4ab972f18f174f3054a37762e558326987bb0
lilkost <111429255+lilkost@users.noreply.github.com>
2024-04-02 22:05:30 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,32 @@
+import { useDynamicAdapt } from '/js/dynamicAdapt.js'
+
+useDynamicAdapt()
+
+if(document.querySelector('.car__slider')) {
+    const parentSlider = document.querySelectorAll('.car__item');
+
+    parentSlider.forEach(parent=>{
+        const currentSlider = parent.querySelector('.car__slider');
+        const currentThumb = parent.querySelector('.car__thumbs');
+        const slides = parent.querySelectorAll('.car__slide');
+
+        const swiper = new Swiper(currentThumb, {
+            spaceBetween: 10,
+            slidesPerView: 4,
+            freeMode: true,
+            watchSlidesProgress: true,
+        });
+        var swiper2 = new Swiper(currentSlider, {
+            spaceBetween: 10,
+            thumbs: {
+                swiper: swiper,
+            },
+        });
+
+        swiper2.on('slideChange', function () {
+            const activeIndex = swiper2.activeIndex;
+            const bg = slides[activeIndex].getAttribute('data-bg');
+            parent.style.backgroundImage = `url(${bg})`;
+        });
+    })
+}
